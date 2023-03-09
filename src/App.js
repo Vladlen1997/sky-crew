b52f20dc3cbffa5b92e7945014c6f779903651e7
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-09 14:39:26 +0300

    Using routing to set the required location of components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from "./Components/Header/Header";
+import NavBar from "./Components/NavBar/NavBar";
+import Profile from "./Components/Profile/Profile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return <BrowserRouter>
+        <div className="app">
+            <Header/>
+            <NavBar/>
+            <div className='profile'>
+                <Routes>
+                   <Route path='/Profile' element={<Profile/>}/>
+                </Routes>
+            </div>
+        </div>
+    </BrowserRouter>
 }
 
 export default App;
