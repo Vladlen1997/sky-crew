6be44a893020ca78bc932c43856ddc2b04c89e2c
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-09 14:36:33 +0300

    Created a profile component and prepared for further filling. Styles added in App component

---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Profile.module.css';
+
+
+const Profile = () => {
+    return (
+        <div className={classes.profile}>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, itaque.
+        </div>
+    )
+}
+
+
+export default Profile;
