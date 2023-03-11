a6982792884c494911d70d1e7de5728d34376c32
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-11 09:43:28 +0300

    added classes and styles

--- src/Components/Header/HeaderLink/HeaderLink.jsx
+++ src/Components/Header/HeaderLink/HeaderLink.jsx
@@ -1,9 +1,10 @@
 import {NavLink} from "react-router-dom";
 import React from "react";
+import classes from './HeaderLink.module.css';
 
 const HeaderLink = (props) => {
     let path = props.id + props.object;
-    return <div>
+    return <div className={classes.links}>
         <NavLink to={path}>{props.object}</NavLink>
     </div>
 }
