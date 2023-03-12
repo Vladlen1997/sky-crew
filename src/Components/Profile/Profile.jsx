ef022c9c9510934e426ef4fa32dfbbff62ea807d
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-12 15:36:08 +0300

    added Posts component and imported

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Profile.module.css';
+import Posts from "./Posts/Posts";
 
 
 const Profile = () => {
     return (
         <div className={classes.profile}>
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, itaque.
+            <Posts />
         </div>
     )
 }
