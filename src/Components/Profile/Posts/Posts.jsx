c4654a493378c6ea882d39a25f9695082077401d
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-15 12:21:27 +0300

    fixed the bug and made catching changes

--- src/Components/Profile/Posts/Posts.jsx
+++ src/Components/Profile/Posts/Posts.jsx
@@ -6,15 +6,23 @@ import Post from "./Post/Post";
 const Posts = (props) => {
 
 
-    let likesCountEl = props.likesCount.map(count => <Post likesCount={count.likesCount} id={count.id}
+    let likesCountEl = props.post.map(count => <Post likesCount={count.likesCount} id={count.id}
                                                            message={count.message}/>)
 
-    let newPostElement = React.createRef();
 
-    const addPost = (props) => {
-        let textRef = newPostElement.current.value;
-        props.addNewPost(textRef);
+    let getRef = React.createRef();
+
+    const addNewPost = () => {
+        let text = getRef.current.value;
+        props.addNewPost(text);
     }
+
+    const onPostChange = () => {
+        let text = getRef.current.value;
+        props.updateNewPostText(text);
+    }
+
+
 
     return <div className={classes.posts}>
         <h3>My Posts</h3>
@@ -24,9 +32,9 @@ const Posts = (props) => {
         </div>
 
         <div>
-            <div><textarea ref={newPostElement} placeholder='write the text...'/></div>
+            <div><textarea onChange={onPostChange} ref={getRef} value={props.newPostText} placeholder='write the text...'/></div>
             <div>
-                <button onClick={addPost}>Add post</button>
+                <button onClick={addNewPost}>Add post</button>
             </div>
         </div>
     </div>
