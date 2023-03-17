b30c522c0f634d2c370ccded153a1ef6a68d1e99
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-17 09:49:33 +0300

    changed rendering

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, {addNewPost, subscribe, updateNewPostText} from "./redux/state";
+import store from "./redux/state";
 import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,16 +11,14 @@ let rerenderEntireTree = (state) => {
     root.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App dialogsPage={state.dialogsPage} post={state.profilePage.post}
-                     nameLink={state.navBarPage.nameLink} headerPage={state.headerPage.headerLink} newPostText={state.profilePage.newPostText} addNewPost={addNewPost}
-              updateNewPostText={updateNewPostText} />
+                <App state={state} dispatch={store.dispatch.bind(store)} />
             </BrowserRouter>
         </React.StrictMode>
     );
 
 }
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
