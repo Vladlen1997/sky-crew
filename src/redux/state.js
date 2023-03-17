bfe6d029ed4529c56ca180deb6f27754ff19a567
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-17 09:49:13 +0300

    Made the logic of sending through messages and a conditional operator. Added store

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,71 +1,97 @@
-let rerenderEntireTree = () => {
-    console.log('state was changed');
-}
 
-let state = {
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-    headerPage: {
-        headerLink: [
-            {id: 1, object: "Shutdown mode"},
-            {id: 2, object: "do not disturb mode"},
-            {id: 3, object: "accessibility mode"}
-        ]
+
+let store = {
+    _callSubscriber() {
+        console.log('state was changed');
     },
 
-    dialogsPage: {
-        dialogs: [
-            {id: 1, dialog: 'Vlad'},
-            {id: 2, dialog: 'Oleg'},
-            {id: 3, dialog: 'Petr'},
-            {id: 4, dialog: 'Fedya'}
-        ],
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 1, message: 'How are you?'},
-            {id: 1, message: 'apsddkd'},
-            {id: 1, message: 'cool'},
-        ],
-    },
-    profilePage: {
-        post: [
-            {id: 1, likesCount: 1, message: 'Hi, how are you?'},
-            {id: 2, likesCount: 12, message: 'What is your name?'},
-            {id: 3, likesCount: 20, message: 'Where are you from?'},
-            {id: 4, likesCount: 6, message: 'Tail is a part of plane'}
-        ],
+    _state: {
 
-        newPostText: ''
+        headerPage: {
+            headerLink: [
+                {id: 1, object: "Shutdown mode"},
+                {id: 2, object: "do not disturb mode"},
+                {id: 3, object: "accessibility mode"}
+            ]
+        },
+
+        dialogsPage: {
+            dialogs: [
+                {id: 1, dialog: 'Vlad'},
+                {id: 2, dialog: 'Oleg'},
+                {id: 3, dialog: 'Petr'},
+                {id: 4, dialog: 'Fedya'}
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 1, message: 'How are you?'},
+                {id: 1, message: 'apsddkd'},
+                {id: 1, message: 'cool'},
+            ],
+        },
+        profilePage: {
+            post: [
+                {id: 1, likesCount: 1, message: 'Hi, how are you?'},
+                {id: 2, likesCount: 12, message: 'What is your name?'},
+                {id: 3, likesCount: 20, message: 'Where are you from?'},
+                {id: 4, likesCount: 6, message: 'Tail is a part of plane'}
+            ],
+
+            newPostText: ''
+        },
+
+        navBarPage: {
+            nameLink: [
+                {id: 1, link: "Profile"},
+                {id: 2, link: "Friends"},
+                {id: 3, link: "Messages"},
+                {id: 4, link: "News"},
+                {id: 5, link: "Settings"}
+            ]
+        }
+
     },
 
-    navBarPage: {
-        nameLink: [
-            {id: 1, link: "Profile"},
-            {id: 2, link: "Friends"},
-            {id: 3, link: "Messages"},
-            {id: 4, link: "News"},
-            {id: 5, link: "Settings"}
-        ]
+    getState() {
+        return this._state;
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
+
+    dispatch(action) {
+
+        if (action.type === ADD_POST) {
+            let newPost = {id: 5, likesCount: 8, message: this._state.profilePage.newPostText};
+            this._state.profilePage.post.push(newPost);
+            this._state.profilePage.newPostText = '';
+            this._callSubscriber(this._state);
+        } else if(action.type === UPDATE_NEW_POST_TEXT) {
+            this._state.profilePage.newPostText = action.text;
+            this._callSubscriber(this._state);
+        }
     }
 
 }
 
 
-export const addNewPost = () => {
-    let newPost = {id: 5, likesCount: 8, message: state.profilePage.newPostText};
-    state.profilePage.post.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
+export let addPostActionCreator = () => {
+    return {
+        type: ADD_POST
+    }
+}
+export let updateNewPostActionCreator = (text) => {
+    return {
+        type: UPDATE_NEW_POST_TEXT, text: text
+    }
+
+
 }
 
 
-export const updateNewPostText = (newPost) => {
-    state.profilePage.newPostText = newPost;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-
-export default state;
+export default store;
+window.store = store;
