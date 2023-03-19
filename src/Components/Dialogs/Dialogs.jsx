0ca38a8e8acf849a2c81c91d2cd457b178c3ab6a
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-19 18:07:26 +0300

    Removed creators from dialogs and linked them to the container component

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,23 +2,21 @@ import React from 'react';
 import classes from './Dialogs.module.css';
 import DialogItems from "./DialogItems/DialogItems";
 import MessagesItems from "./MessagesItems/MessagesItems";
-import {sendMessageActionCreator, updateNewMessageBodyActionCreator} from "../../redux/dialogs-reducer";
-
-
 
 const Dialogs = (props) => {
 
     let dialogsEl = props.dialogsPage.dialogs.map(dialog => <DialogItems id={dialog.id} dialog={dialog.dialog}/>)
-    let messagesEl = props.dialogsPage.messages.map(message => <MessagesItems id={message.id} message={message.message}/>)
+    let messagesEl = props.dialogsPage.messages.map(message => <MessagesItems id={message.id}
+                                                                              message={message.message}/>)
 
 
     const addMessage = () => {
-        props.dispatch(sendMessageActionCreator());
+        props.sendMessage();
     }
 
     const newMessageChange = (e) => {
         let object = e.target.value;
-        props.dispatch(updateNewMessageBodyActionCreator(object));
+        props.updateNewMessageBody(object);
 
     }
 
@@ -33,7 +31,8 @@ const Dialogs = (props) => {
 
             <div>
                 <div>
-                    <textarea onChange={newMessageChange} value={props.dialogsPage.newMessageText} placeholder='Write message...'/>
+                    <textarea onChange={newMessageChange} value={props.dialogsPage.newMessageText}
+                              placeholder='Write message...'/>
                 </div>
                 <div>
                     <button onClick={addMessage}>Send message</button>
