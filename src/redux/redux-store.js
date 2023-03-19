ab832c26f263783fa0efbd6f753f377fb0c566ee
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-19 10:52:32 +0300

    created redux-store file, specified reducers and created a store

---
+++ src/redux/redux-store.js
@@ -0,0 +1,16 @@
+import {combineReducers, legacy_createStore as createStore} from "redux";
+import headerReducer from "./header-reducer";
+import navBarReducer from "./navBar-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import profileReducer from "./profile-reducer";
+
+let reducers = combineReducers({
+    headerPage: headerReducer,
+    navBarPage: navBarReducer,
+    dialogsPage: dialogsReducer,
+    profilePage: profileReducer
+});
+
+let store = createStore(reducers);
+
+export default store;
