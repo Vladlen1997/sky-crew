28baea98b469ca7775fda46b35a2c138c4b9663e
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-20 11:47:36 +0300

    Changed Presentation Components to Container Components in App Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import {Route, Routes} from "react-router-dom";
-import Header from "./Components/Header/Header";
-import NavBar from "./Components/NavBar/NavBar";
 import Profile from "./Components/Profile/Profile";
 import DialogsContainer from "./Components/Dialogs/DialogsContainer/DialogsContainer";
+import HeaderContainer from "./Components/Header/HeaderContainer/HeaderContainer";
+import NavBarContainer from "./Components/NavBar/NavBarContainer/NavBarContainer";
 
 
 const App = () => {
 
     return (
         <div className="app">
-            <Header />
-            <NavBar />
+            <HeaderContainer />
+            <NavBarContainer />
             <div className='profile'>
                 <Routes>
                     <Route path='/Profile'
