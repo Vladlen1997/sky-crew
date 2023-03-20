48461e754055e9bae730a0231171c0bcf07e04e8
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-20 11:46:48 +0300

    Added a StoreContext to the container component and called a function which accepts store and returns updated data

--- src/Components/Dialogs/DialogsContainer/DialogsContainer.jsx
+++ src/Components/Dialogs/DialogsContainer/DialogsContainer.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import {sendMessageActionCreator, updateNewMessageBodyActionCreator} from "../../../redux/dialogs-reducer";
 import Dialogs from "../Dialogs";
+import StoreContext from "../../../StoreContext";
 
-const DialogsContainer = (props) => {
-
-    let state = props.store.getState().dialogsPage;
-
-    let addMessage = () => {
-        props.store.dispatch(sendMessageActionCreator());
-    }
-
-    let newMessageChange = (object) => {
-        props.store.dispatch(updateNewMessageBodyActionCreator(object));
-
-    }
+const DialogsContainer = () => {
 
     return (
-        <Dialogs sendMessage = {addMessage} updateNewMessageBody = {newMessageChange} dialogsPage = {state}/>
+        <StoreContext.Consumer>
+            {
+            (store) => {
+                let state = store.getState().dialogsPage;
+                let addMessage = () => {
+                    store.dispatch(sendMessageActionCreator());
+                }
+                let newMessageChange = (object) => {
+                    store.dispatch(updateNewMessageBodyActionCreator(object));
+
+                }
+                return <Dialogs sendMessage={addMessage} updateNewMessageBody={newMessageChange} dialogsPage={state}/>
+            }
+        }
+        </StoreContext.Consumer>
     )
 }
 
