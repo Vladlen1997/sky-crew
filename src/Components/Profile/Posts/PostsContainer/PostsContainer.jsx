7805f4ee87f973763fe51eab792ae096f18b47fa
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-21 13:37:32 +0300

    accessed the entire profilePage object

--- src/Components/Profile/Posts/PostsContainer/PostsContainer.jsx
+++ src/Components/Profile/Posts/PostsContainer/PostsContainer.jsx
@@ -6,7 +6,7 @@ import {connect} from "react-redux";
 
 const mapStateToProps = (state) => {
     return {
-        profilePage: state.profilePage.post
+        profilePage: state.profilePage
     }
 }
 
