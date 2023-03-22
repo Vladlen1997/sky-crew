9e75d8c70c63860ca67be8e5a9bcc7d67322a468
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-22 12:11:07 +0300

    created a Users presentational component and added a UI

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import classes from './Users.module.css';
 
 
-const Users = () => {
+const Users = (props) => {
+
     return (
-        <div>Esers will be here</div>
+        <div>
+            {
+                props.users.map(users =>
+                    <div key={users.id}>
+                        <span>
+                            <div>
+                                <img src={users.photoUrl} alt="Photo" className={classes.userPhoto}/>
+                            </div>
+                            <div>
+                                {users.followed
+                                    ? <button onClick={() => {props.UNFOLLOW(users.id) } }>Unfollow</button>
+                                    : <button onClick={() => {props.FOLLOW(users.id)}}>Follow</button> }
+                            </div>
+                        </span>
+                        <span>
+                            <span>
+                                <div>{users.fullName}</div>
+                                <div>{users.status}</div>
+                            </span>
+
+                            <span>
+                                <div>{users.location.country}</div>
+                                <div>{users.location.city}</div>
+                            </span>
+                        </span>
+                    </div>)
+            }
+        </div>
     )
 }
 
