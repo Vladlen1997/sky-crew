a0edc99d026780ff6e963510a4730ae88dfa5296
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-27 16:06:27 +0300

    added users to navigate bar

--- src/redux/navBar-reducer.js
+++ src/redux/navBar-reducer.js
@@ -5,7 +5,8 @@ let initialState = {
         {id: 2, link: "Friends"},
         {id: 3, link: "Messages"},
         {id: 4, link: "News"},
-        {id: 5, link: "Settings"}
+        {id: 5, link: "Settings"},
+        {id: 6, link: "Users"}
     ]
 };
 
