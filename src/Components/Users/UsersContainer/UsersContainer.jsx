5ca21e45950a1291f2af6882f0f83b0c5f27ad15
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-03-28 21:14:10 +0300

    passed currentPage through mapstate

--- src/Components/Users/UsersContainer/UsersContainer.jsx
+++ src/Components/Users/UsersContainer/UsersContainer.jsx
@@ -7,7 +7,8 @@ let mapStateToProps = (state) => {
     return {
         users: state.usersPage.users,
         pageSize: state.usersPage.pageSize,
-        totalCount: state.usersPage.totalUsersCount
+        totalCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 
