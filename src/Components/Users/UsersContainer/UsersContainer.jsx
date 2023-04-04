34aeafd97431595ecc93e1de5eedc942e5773bce
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-04-04 10:45:16 +0300

    threw actions, moved the class with ajax requests to the container component

--- src/Components/Users/UsersContainer/UsersContainer.jsx
+++ src/Components/Users/UsersContainer/UsersContainer.jsx
@@ -1,6 +1,34 @@
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../../redux/users-reducer";
+import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../../redux/users-reducer";
+import React from "react";
+import axios from "axios";
 import Users from "../Users";
+
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
+        })
+    }
+
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+        })
+    }
+
+    render() {
+        return <Users totalCount={this.props.totalCount} pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      onPageChanged={this.onPageChanged} users={this.props.users} follow={this.props.follow}
+                      unfollow={this.props.unfollow}/>
+    }
+}
 
 
 let mapStateToProps = (state) => {
@@ -26,13 +54,18 @@ let mapDispatchToProps = (dispatch) => {
 
         setUsers: (users) => {
             dispatch(setUsersAC(users))
+        },
+
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+
+        setTotalUsersCount: (usersCount) => {
+            dispatch(setTotalUsersCountAC(usersCount))
         }
 
     }
 }
 
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
