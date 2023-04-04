86695b814c270bf4581ea911a8fbd05e70421fe4
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-04-04 10:40:13 +0300

    Made the user component clean, moved the ajax requests to the container

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,73 +1,67 @@
 import React from 'react';
 import classes from './Users.module.css';
-import axios from "axios";
 import user from '../../assets/images/user.png';
 
-class Users extends React.Component {
 
-    componentDidMount() {
-        axios.get("https://637b689d10a6f23f7fa7ea2b.mockapi.io/Items").then(response => {
-            this.props.setUsers(response.data)
-        })
+const Users = (props) => {
+
+    let pagesCount = Math.ceil(props.totalCount / props.pageSize); //pagesCount=20p/5p
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+
     }
 
-    render() {
+    return (
 
-        let pagesCount = Math.ceil(this.props.totalCount / this.props.pageSize)
-        let pages = [];
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return (
-
-
+        <div>
             <div>
-                <div>
-                    {pages.map(p => {
-                        return <span className={this.props.currentPage === p && classes.selectedPage}>{p}</span>
-                    })}
+                {pages.map(p => {
+                    return <span className={props.currentPage === p && classes.selectedPage} onClick={() => {
+                        props.onPageChanged(p)
+                    }}>{p}</span>
+                })}
 
-                </div>
-                {
-                    this.props.users.map(u => <div key={u.id}>
+            </div>
+            {
+                props.users.map(u => <div key={u.id}>
 
                         <span>
                             <div className={classes.photo}>
-                                <img src={u.photoUrl != null ? u.photoUrl : user} alt="face"/>
+                                <img src={u.photos.small != null ? u.photos.small : user} alt="face"/>
                             </div>
+
 
                             <div className="subscribe">
 
                                 {
                                     u.followed
                                         ? <button onClick={() => {
-                                            this.props.unfollow(u.id)
+                                            props.unfollow(u.id)
                                         }}>unfollow</button>
                                         : <button onClick={() => {
-                                            this.props.follow(u.id)
+                                           props.follow(u.id)
                                         }}>follow</button>
                                 }
                             </div>
 
                             </span>
+                        <span>
                             <span>
-                            <span>
-                                <div>{u.fullName}</div><div><div>{u.status}</div></div>
+                                <div>{u.name}</div><div><div>{u.status}</div></div>
                             </span>
                         </span>
 
-                            <span className="cityBlock">
-                                <div className="country">{u.location.country}</div>
-                                <div className="city">{u.location.city}</div>
+                        <span className="cityBlock">
+                                <div className="country">{'u.location.country'}</div>
+                                <div className="city">{'u.location.city'}</div>
                             </span>
-                        </div>
-                    )}
+                    </div>
+                )}
 
-            </div>
-        )
+        </div>
+    )
 
-    }
 }
 
 
