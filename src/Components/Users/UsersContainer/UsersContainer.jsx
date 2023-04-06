42ef66f783ffd20b4353217f496e55b177f8d351
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-04-06 10:24:56 +0300

    passing through a container component

--- src/Components/Users/UsersContainer/UsersContainer.jsx
+++ src/Components/Users/UsersContainer/UsersContainer.jsx
@@ -36,7 +36,8 @@ let mapStateToProps = (state) => {
         users: state.usersPage.users,
         pageSize: state.usersPage.pageSize,
         totalCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage
+        currentPage: state.usersPage.currentPage,
+        isFetching: state.usersPage.isFetching
     }
 }
 
