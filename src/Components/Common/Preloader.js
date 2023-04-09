4a12788266ffc43a2d7f04859df89e8ec37eb37c
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-04-09 13:14:52 +0300

    renamed Common to Preloader, moved information from users Container

---
+++ src/Components/Common/Preloader.js
@@ -0,0 +1,9 @@
+import preloader from "../../assets/images/Bars-1s-200px.svg";
+
+let Preloader = (props) => {
+    return <div style={{backgroundColor: "whitesmoke"}}>
+        <img src={preloader}/>
+    </div>
+}
+
+export default Preloader;
