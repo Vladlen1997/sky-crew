b24423c254d5a0d973e5577b63caca3c54e2de53
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-04-21 12:14:52 +0300

    specified and imported profileInfo

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import classes from './Profile.module.css';
 import PostsContainer from "./Posts/PostsContainer/PostsContainer";
+import ProfileInfo from "./ProfileInfo";
 
 
 const Profile = (props) => {
 
     return (
         <div className={classes.profile}>
-            <PostsContainer />
+            <ProfileInfo profile={props.profile}/>
+            <PostsContainer/>
         </div>
     )
 }
